855a240a62799280ce53baab903d977787ce6d49
Karthik <karthiksp1911@gmail.com>
2026-02-18 18:54:14 +0530

    feat: initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/fastapi")
+      .then(res => setData(res.data.message))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>{data}</h1>
+    </div>
+  );
+}
+
+export default App;
